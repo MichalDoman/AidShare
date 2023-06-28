4e8b602eed90e64951d2daf83f4ef376cda9222d
michaldoman <mjd.domanski@gmail.com>
2023-06-28 18:09:48 +0200

    set_up

---
+++ AidShare/AidShare/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for AidShare project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AidShare.settings')
+
+application = get_wsgi_application()
