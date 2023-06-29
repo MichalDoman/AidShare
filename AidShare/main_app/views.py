4f69f345e98ea9fcef6f7a19ac73ef2d3fe92f12
michaldoman <mjd.domanski@gmail.com>
2023-06-29 19:06:47 +0200

    Landing page

--- AidShare/main_app/views.py
+++ AidShare/main_app/views.py
@@ -1,10 +1,38 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.generic import ListView
+
+from main_app.models import Donation, Institution
 
 
-class HomeView(View):
-    def get(self, request):
-        return render(request, "index.html")
+class HomeView(ListView):
+    """Landing Page of the app."""
+    model = Donation
+    template_name = "donation_index.html"
+    template_name_suffix = "_index"
+    context_object_name = "donations"
+    paginate_by = 5
+
+    def get_context_data(self, **kwargs):
+        """Add institutions objects, number of bags and supported institutions count to the context."""
+        context = super().get_context_data()
+        user = self.request.user
+        donations = Donation.objects.filter(user=user.id)
+        institutions = Institution.objects.all()
+        context['institutions'] = institutions
+
+        sum_of_bags = 0
+        for donation in donations:
+            sum_of_bags += donation.quantity
+
+        context['bags'] = sum_of_bags
+
+        institutions_count = []
+        for donation in donations:
+            institutions.append(donation.institution)
+
+        context['institutions_count'] = len(set(institutions_count))
+        return context
 
 
 class AddDonationView(View):
